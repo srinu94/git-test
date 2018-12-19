8ebc6717fbe114455f7bf67bb8a2f8fffdfe21af
srinu94 <multiuse94@gmail.com>
2018-12-19 17:22:02 +0000

    modified

--- sample.groovy
+++ sample.groovy
@@ -22,7 +22,7 @@ pipeline {
                         }
                 }
                 steps {
-			echo "Hello"
+			 echo "Hello"
                       }
         }
                                
